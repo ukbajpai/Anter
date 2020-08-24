b54c219d3651ff2782d8dc911fed3905dca8aca2
Utkarsh Bajpai <utkarsh.bajpai@oracle.com>
2020-08-24 22:22:58 +0530

    intial commit

---
+++ src/main/java/com/encapsulateideas/anter/Anter/AnterApplication.java
@@ -0,0 +1,13 @@
+package com.encapsulateideas.anter.Anter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnterApplication.class, args);
+	}
+
+}
